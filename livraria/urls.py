0da65f3c2641f4b1b5802ada31949a38d3a2dccd
juliacaetano <juliacaetano@gmail.com>
2022-10-13 16:26:16 -0300

    commit 14 - Autenticação com o SimpleJWT

--- livraria/urls.py
+++ livraria/urls.py
@@ -6,6 +6,11 @@ from rest_framework.routers import DefaultRouter
 
 from core.views import CategoriaViewSet, AutorViewSet, EditoraViewSet, LivroViewSet
 
+from rest_framework_simplejwt.views import (
+    TokenObtainPairView,
+    TokenRefreshView,
+)
+
 router = DefaultRouter()
 router.register(r"Categorias", CategoriaViewSet)
 router.register(r"Autores", AutorViewSet)
@@ -15,4 +20,6 @@ router.register(r"Livros", LivroViewSet)
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("", include(router.urls)),
+    path("token/", TokenObtainPairView.as_view(), name="token_obtain_pair"),
+    path("token/refresh/", TokenRefreshView.as_view(), name="token_refresh"),
 ]
